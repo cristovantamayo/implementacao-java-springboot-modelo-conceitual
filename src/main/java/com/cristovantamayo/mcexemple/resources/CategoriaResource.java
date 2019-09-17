4caa16d17463dfc55223085ac89ef7fd88e948d8
Cristovan Tamayo <cristovan.tamayo@gmail.com>
2019-09-17 15:16:50 -0300

    Domain class added - Categoria

--- src/main/java/com/cristovantamayo/mcexemple/resources/CategoriaResource.java
+++ src/main/java/com/cristovantamayo/mcexemple/resources/CategoriaResource.java
@@ -1,15 +1,28 @@
 package com.cristovantamayo.mcexemple.resources;
 
+import java.util.List;
+import java.util.ArrayList;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cristovantamayo.mcexemple.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listat() {
-		return "REST está funcionando!";
+	public List<Categoria> listat() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 }
