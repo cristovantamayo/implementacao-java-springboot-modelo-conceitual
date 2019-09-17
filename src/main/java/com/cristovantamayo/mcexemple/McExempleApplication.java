a74dea3b37f7d8d7a86020aa0320773cba52e8f7
Cristovan Tamayo <cristovan.tamayo@gmail.com>
2019-09-17 14:49:07 -0300

    Projeto Criado

---
+++ src/main/java/com/cristovantamayo/mcexemple/McExempleApplication.java
@@ -0,0 +1,13 @@
+package com.cristovantamayo.mcexemple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McExempleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McExempleApplication.class, args);
+	}
+
+}
