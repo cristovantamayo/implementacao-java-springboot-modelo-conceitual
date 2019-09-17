fc5a01200dd81e7694690b12fcc3c2887c233538
Cristovan Tamayo <cristovan.tamayo@gmail.com>
2019-09-17 17:52:43 -0300

    H2 Database and JPA Anotations Added

--- src/main/java/com/cristovantamayo/mcexemple/domain/Categoria.java
+++ src/main/java/com/cristovantamayo/mcexemple/domain/Categoria.java
@@ -2,11 +2,20 @@ package com.cristovantamayo.mcexemple.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable {
 	
 	private static final long serialVersionUID = -8358657423842141661L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
+	
 	private String nome;
 	
 	public Categoria() {
