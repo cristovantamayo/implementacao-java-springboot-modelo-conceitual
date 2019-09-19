ab565ce583924e7943eae2cd0549417c24d5aaff
Cristovan Tamayo <cristovan.tamayo@gmail.com>
2019-09-19 09:21:49 -0300

    Produto Domain and association Many to Many with Categoria Domain

--- src/main/java/com/cristovantamayo/mcexemple/McExempleApplication.java
+++ src/main/java/com/cristovantamayo/mcexemple/McExempleApplication.java
@@ -1,13 +1,51 @@
 package com.cristovantamayo.mcexemple;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.cristovantamayo.mcexemple.domain.Categoria;
+import com.cristovantamayo.mcexemple.domain.Produto;
+import com.cristovantamayo.mcexemple.repositories.CategoriaRepository;
+import com.cristovantamayo.mcexemple.repositories.ProdutoRepository;
+
 @SpringBootApplication
-public class McExempleApplication {
+public class McExempleApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(McExempleApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse Lazer", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.setCategorias(Arrays.asList(cat1));
+		p2.setCategorias(Arrays.asList(cat1, cat2));
+		p3.setCategorias(Arrays.asList(cat1));
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
 	}
 
 }
--- src/main/java/com/cristovantamayo/mcexemple/domain/Categoria.java
+++ src/main/java/com/cristovantamayo/mcexemple/domain/Categoria.java
@@ -1,11 +1,14 @@
 package com.cristovantamayo.mcexemple.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Categoria implements Serializable {
@@ -15,8 +18,10 @@ public class Categoria implements Serializable {
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
-	
 	private String nome;
+	
+	@ManyToMany(mappedBy = "categorias")
+	private List<Produto> produtos = new ArrayList<>();
 	
 	public Categoria() {
 		
@@ -43,6 +48,14 @@ public class Categoria implements Serializable {
 		this.nome = nome;
 	}
 
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+	
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
+	
 	@Override
 	public String toString() {
 		return "Categoria [id=" + id + ", nome=" + nome + "]";
@@ -72,5 +85,4 @@ public class Categoria implements Serializable {
 			return false;
 		return true;
 	}
-	
 }
