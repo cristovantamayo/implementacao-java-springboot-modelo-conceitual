cae06e5edaa6f5ca4272280e595eb2ab5ac80abd
Cristovan Tamayo <cristovan.tamayo@gmail.com>
2019-09-19 10:54:57 -0300

    Applying categorias/{id} endpoint exception handling

--- src/main/java/com/cristovantamayo/mcexemple/services/CategoriaService.java
+++ src/main/java/com/cristovantamayo/mcexemple/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.cristovantamayo.mcexemple.domain.Categoria;
 import com.cristovantamayo.mcexemple.repositories.CategoriaRepository;
+import com.cristovantamayo.mcexemple.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,6 +17,9 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
+		if(obj.isEmpty()) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName());
+		}
 		return obj.get();
 	}
 }
